bc9c83bc4e31ef954d8f56e33d405fc654ff328d
maliamar11 <93571600+maliamar11@users.noreply.github.com>
2021-11-03 14:38:10 +0530

    Add files via upload

---
+++ ReportetLog.java
@@ -0,0 +1,29 @@
+package Methods;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ReportetLog {
+	@Test
+public void Open() {
+		System.out.println("open the browser");
+		Reporter.log("Browser Launch Successfully",true);
+		}
+ @Test(enabled=false)
+ public void login() {
+	 System.out.println("Login Sucessfully");
+ }
+ @Test(groups = {"sanity"})
+ public void A() {
+	 System.out.println("It is method A");
+ }
+ @Test(groups = {"sanity"})
+ public void B(){
+	 System.out.println("it is method B"); }
+	
+ @Test(groups={"regression"})
+ public void C() {
+	 System.out.println("It is method C");
+	 }
+ 
+ }
